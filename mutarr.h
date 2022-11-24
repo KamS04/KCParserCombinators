1ae6430e9ba17a16ea1f63d0902f4740ea03a199
KamS04 <singhkam07@gmail.com>
2022-11-24 03:13:48 -0500

    Added proper Nullability to mutarr

--- mutarr.h
+++ mutarr.h
@@ -1,8 +1,12 @@
 #include<stdlib.h>
 #include<string.h>
 
+// specify nullable alloc
+// specific null alloc
+// normal alloc
+
 #define MUTARR(x) _##x##_arr
-#define NULX(x) x##nul
+#define NULX(x) __##x##nul
 #define ARR_SIZE(x) _##x##_as
 #define CARR_SIZE(x) _##x##_cal
 #define T_ARR(x) _##x##_tarr
@@ -10,13 +14,26 @@
 #define NSIZE(x) _##x##ns
 #define ATYPE(x) x##_type
 #define TINT(x) _##x##t_int
-#define ALLOCATE(x, ss) int ARR_SIZE(x) = ss; \
+#define SNULL(x) _##x##set_null
+#define NULLIFY(x) NULLIFY_S(x, 0)
+#define NULLIFY_S(x, st) if (SNULL(x)) { \
+                            for (TINT(x) = st; TINT(x) < ARR_SIZE(x); TINT(x)++) { \
+                                MUTARR(x)[TINT(x)] = NULX(x); \
+                            } \
+                        }
+// identifier, startsize, setnull, null
+#define ALLOCATE(x, ss) ALLOCATE_SN(x, ss, 0, 0)
+#define ALLOCATE_S(x, ss, sn) ALLOCATE_SN(x, ss, sn, NULL)
+#define ALLOCATE_SN(x, ss, sn, n) int ARR_SIZE(x) = ss; \
                         int SSIZE(x) = ss; \
                         int CARR_SIZE(x) = 0; \
                         int NSIZE(x); \
                         int TINT(x); \
+                        int SNULL(x) = sn; \
+                        ATYPE(x) NULX(x) = n; \
                         ATYPE(x)* T_ARR(x); \
-                        ATYPE(x)* MUTARR(x) = malloc( ss * sizeof(ATYPE(x)));
+                        ATYPE(x)* MUTARR(x) = malloc( ss * sizeof(ATYPE(x))); \
+                        NULLIFY(x);
 #define CHECK_REALLOC_NEEDED(x) CARR_SIZE(x) == ARR_SIZE(x)
 #define CHECK_SP_REALLOC_NEEDED(x, n) CARR_SIZE(x) + n >= ARR_SIZE(x)
 #define REALLOCATE(x, nsi) NSIZE(x) = ARR_SIZE(x) + nsi; \
@@ -25,7 +42,8 @@
                         free(MUTARR(x)); \
                         MUTARR(x) = T_ARR(x); \
                         ARR_SIZE(x) = NSIZE(x); \
-                        T_ARR(x) = NULL;
+                        T_ARR(x) = NULL; \
+                        NULLIFY_S(x, CARR_SIZE(x)+1);
 #define REALLOCATE_NMIN(x, nmsi) T_INT(x) = (CARR_SIZE(x) + nmsi) - ARR_SIZE; \
                                 T_INT(x) += SSIZE(x) - (T_INT % SSIZE(x)); \
                                 REALLOCATE(x, T_INT(x))
@@ -40,15 +58,13 @@
                                 free(MUTARR(x)); \
                                 MUTARR(x) = T_ARR(x); \
                             }
-#define CLEAR(x, null) for (int i = 0; i < ARR_SIZE(x); i++) {\
-                    MUTARR(x)[i] = null; \
-                } \
+#define CLEAR(x) NULLIFY(x); \
                 CARR_SIZE(x) = 0;
-#define CLEAR_SHRINK_N(x, null) T_ARR(x) = malloc( SSIZE(x) * sizeof(ATYPE(x)*) ); \
+#define CLEAR_SHRINK(x) T_ARR(x) = malloc( SSIZE(x) * sizeof(ATYPE(x)*) ); \
                         free(MUTARR(x)); \
                         MUTARR(x) = T_ARR(x); \
                         T_ARR(x) = NULL; \
                         ARR_SIZE(x) = SSIZE(x); \
-                        CARR_SIZE(x) = 0;
-#define CLEAR_SHRINK(x) CLEAR_SHRINK_N(x, NULL)
+                        CARR_SIZE(x) = 0; \
+                        NULLIFY(x);
 #define SIZEOF(x) CARR_SIZE(x)
