27c8bd86f8bb311601f9dd8c3ec736cc5535be36
KamS04 <singhkam07@gmail.com>
2022-11-25 17:06:50 -0500

    Updated make/build system

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-gcc %* parsers.h parsers.c state.h state.c corolib.h corolib.c ^
-    state_default_dealloc.h state_default_dealloc.c util.h util.c ^
-    character_p.h character_p.c comp_p.h comp_p.c core_p.h core_p.c ^
-    mutarr.h string_p.h string_p.c util_p.h util_p.c
+gcc %* parsers.c state.c corolib.c ^
+    state_default_dealloc.c util.c ^
+    character_p.c comp_p.c core_p.c ^
+    string_p.c util_p.c re.c
