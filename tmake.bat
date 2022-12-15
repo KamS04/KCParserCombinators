8e3f0e5d534758b99086f0df87097f4d9b14cfea
KamS04 <singhkam07@gmail.com>
2022-12-15 18:21:45 -0500

    Echo off in make file + tmake where -o is test.exe

--- tmake.bat
+++ tmake.bat
@@ -1,6 +1,4 @@
-@echo off
-
 gcc %* parsers.c state.c korolib.c ^
     state_default_dealloc.c util.c ^
     character_p.c comp_p.c core_p.c ^
-    string_p.c util_p.c kc_config.c re.c
+    string_p.c util_p.c re.c -o test.exe
