d88e114c3ee4885ab48aea3034bf061053cccda4
KamS04 <singhkam07@gmail.com>
2022-12-15 17:20:34 -0500

    Added kc_config.c/h to make/build scripts

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo off
 
-gcc -c parsers.h parsers.c state.h state.c corolib.h corolib.c ^
+gcc -c parsers.h parsers.c state.h state.c korolib.h korolib.c ^
     state_default_dealloc.h state_default_dealloc.c util.h util.c ^
     character_p.h character_p.c comp_p.h comp_p.c core_p.h core_p.c ^
-    mutarr.h string_p.h string_p.c util_p.h util_p.c re.h re.c
+    mutarr.h string_p.h string_p.c util_p.h util_p.c kc_config.h kc_config.c re.h re.c
 
-ar rcs %* parsers.o state.o corolib.o state_default_dealloc.o ^
+ar rcs %* parsers.o state.o korolib.o state_default_dealloc.o ^
                     util.o character_p.o comp_p.o core_p.o string_p.o ^
-                    util_p.o re.o
+                    util_p.o kc_config.o re.o
 del *.o
 del *.gch
--- make
+++ make
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc $* parsers.c state.c corolib.c \
+gcc $* parsers.c state.c korolib.c \
     state_default_dealloc.c util.c \
     character_p.c comp_p.c core_p.c \
-    string_p.c util_p.c re.c \
+    string_p.c util_p.c kc_config.c re.c \
     -lmath
